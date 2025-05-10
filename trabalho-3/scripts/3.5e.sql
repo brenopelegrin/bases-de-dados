16ad37d74e0b59ffaffb9d225a003b849358639d
gabriel.akio <gabriel.urakawa@gmail.com>
2025-05-10 20:43:07 -0300

    fix: queries 3.5 d and e logic

--- trabalho-3/scripts/3.5e.sql
+++ trabalho-3/scripts/3.5e.sql
@@ -1,7 +1,7 @@
 SELECT U_loc.nome, U_loc.sobrenome, U_loc.cpf
 FROM Usuario AS U_loc
 WHERE U_loc.locatario = true 
-  AND U_loc.data_nascimento < (
+  AND U_loc.data_nascimento > (
     SELECT MIN(U_alf.data_nascimento)
     FROM Usuario AS U_alf
     WHERE U_alf.locador = true
