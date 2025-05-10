5d3d7d5a902e11d37dd91aeede4e1c7a186f8816
gabriel.akio <gabriel.urakawa@gmail.com>
2025-05-10 18:25:18 -0300

    feat: queries 3.5d and 3.5e

--- trabalho-3/scripts/3.5d.sql
+++ trabalho-3/scripts/3.5d.sql
@@ -1,6 +1,8 @@
-select nome,data_nascimento from 
-usuario where data_nascimento > (
-	select data_nascimento
-	from usuario
-	where usuario.nome='Leandro' and usuario.locador=True
-) and locatario=True
+SELECT U_loc.nome, U_loc.sobrenome, U_loc.cpf
+FROM Usuario AS U_loc
+WHERE U_loc.locatario = true 
+  AND U_loc.data_nascimento < (
+    SELECT MAX(U_alf.data_nascimento)
+    FROM Usuario AS U_alf
+    WHERE U_alf.locador = true
+  );
--- trabalho-3/scripts/3.5e.sql
+++ trabalho-3/scripts/3.5e.sql
@@ -1,6 +1,8 @@
-select nome,data_nascimento from 
-usuario where data_nascimento >= ALL (
-	select data_nascimento
-	from usuario
-	where usuario.locador=True
-) and locatario=True
+SELECT U_loc.nome, U_loc.sobrenome, U_loc.cpf
+FROM Usuario AS U_loc
+WHERE U_loc.locatario = true 
+  AND U_loc.data_nascimento < (
+    SELECT MIN(U_alf.data_nascimento)
+    FROM Usuario AS U_alf
+    WHERE U_alf.locador = true
+  );
