bdb8d3ed672026cafb1e265b03a2ac5608e0c96e
gabriel.akio <gabriel.urakawa@gmail.com>
2025-05-10 19:35:32 -0300

    feat: query 3.5c

---
+++ trabalho-3/scripts/3.5c.sql
@@ -0,0 +1,8 @@
+SELECT EXTRACT(MONTH FROM R.data_reserva) as mes_reserva, R.status as confirmacao, AVG(R.preco_total_estadia)/100 as media_diaria
+FROM Reserva as R
+JOIN Propriedade as P
+ON P.id_propriedade = R.id_propriedade
+JOIN Localizacao as L
+ON P.id_localizacao = L.id_localizacao
+Group by EXTRACT(MONTH FROM R.data_reserva), R.status
+Order by mes_reserva
--- trabalho-3/scripts/3.5d.sql
+++ trabalho-3/scripts/3.5d.sql
@@ -4,5 +4,5 @@ WHERE U_loc.locatario = true
   AND U_loc.data_nascimento < (
     SELECT MAX(U_alf.data_nascimento)
     FROM Usuario AS U_alf
-    WHERE U_alf.locador = true
+    WHERE U_alf.locador = true	
   );
